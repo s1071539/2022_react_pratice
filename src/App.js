62dafabaec7ef0c943b51fbbd81ac4a5185b267e
pseuder <iop890520@gmail.com>
2022-10-19 20:50:31 +0800

    2F

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import { Routes, Route } from "react-router-dom";
+
+import "./styles/style.css";
 
 const App = () => {
   return (
     <div className="App">
       <Nav />
-      <HomePage />
+      <Routes>
+        <Route path="/" exact element={<HomePage />} />
+        <Route path="/about" exact element={<About />} />
+      </Routes>
+
       <Footer />
     </div>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,12 @@ import Search from "../components/Search";
 
 const HomePage = () => {
   const auth = "563492ad6f917000010000019f7093a3b04c4d5bafbfd99c5573ccc1";
-  return <div>HomePage</div>;
+  return (
+    <div>
+      {" "}
+      <h1>HomePage</h1> <Search />{" "}
+    </div>
+  );
 };
 
 export default HomePage;
